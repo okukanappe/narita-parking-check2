3111afbb0b22ffe07ee9a67bd0470956490521f7
okukanappe <okukana@gmail.com>
2026-07-09 09:19:18 +0900

    Update notify.py

--- notify.py
+++ notify.py
@@ -1,1 +1,17 @@
-# 後で書きます
+import os
+import requests
+
+USER = os.environ["PUSHOVER_USER"]
+TOKEN = os.environ["PUSHOVER_TOKEN"]
+
+requests.post(
+    "https://api.pushover.net/1/messages.json",
+    data={
+        "token": TOKEN,
+        "user": USER,
+        "title": "GitHub Actions",
+        "message": "🎉 テスト通知です！",
+    },
+)
+
+print("通知を送信しました")
